82f3dd08e603debde8c590c18dc287913622a6b3
Florent <florent.escots@gmail.com>
2022-07-18 08:17:11 +0200

    api complete ( get post delete update ..)

---
+++ models/things.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+const mangoose = require("mongoose");
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("things", thingSchema);
